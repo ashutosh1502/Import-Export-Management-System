34cbd3eff4194037768417b38167962beb842cd0
Ashutosh Sawant <ashutoshsawant1502@gmail.com>
2025-04-15 09:23:29 +0530

    Update: Added getter for fields

--- src/main/java/com/project/models/ExportStatement.java
+++ src/main/java/com/project/models/ExportStatement.java
@@ -3,15 +3,15 @@ package com.project.models;
 import java.util.ArrayList;
 
 public class ExportStatement {
-    public String industryName,address,phoneNumber;
+    public String industryName,address, contact;
     public String customerName,customerAddress,customerPhoneNumber;
     public ArrayList<ExportStatementTableEntry> tableEntries;
     public double grandTotal, totalPending, totalPaid;
 
-    public ExportStatement(String industryName, String address, String phoneNumber, String customerName, String customerAddress, String customerPhoneNumber, ArrayList<ExportStatementTableEntry> tableEntries, double grandTotal, double totalPending, double totalPaid) {
+    public ExportStatement(String industryName, String address, String contact, String customerName, String customerAddress, String customerPhoneNumber, ArrayList<ExportStatementTableEntry> tableEntries, double grandTotal, double totalPending, double totalPaid) {
         this.industryName = industryName;
         this.address = address;
-        this.phoneNumber = phoneNumber;
+        this.contact = contact;
         this.customerName = customerName;
         this.customerAddress = customerAddress;
         this.customerPhoneNumber = customerPhoneNumber;
@@ -19,5 +19,45 @@ public class ExportStatement {
         this.grandTotal = grandTotal;
         this.totalPending = totalPending;
         this.totalPaid = totalPaid;
+    }
+
+    public String getIndustryName() {
+        return industryName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+
+    public ArrayList<ExportStatementTableEntry> getTableEntries() {
+        return tableEntries;
+    }
+
+    public double getGrandTotal() {
+        return grandTotal;
+    }
+
+    public double getTotalPending() {
+        return totalPending;
+    }
+
+    public double getTotalPaid() {
+        return totalPaid;
     }
 }
